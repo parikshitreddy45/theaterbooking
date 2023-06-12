8d849f817451773e798d11ba587fa3254759c446
parikshit <parikshitreddy37@gmail.com>
2023-06-12 17:31:47 +0530

    first commit

--- node/bookmyshowapi/app.js
+++ node/bookmyshowapi/app.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express();
-let port = 9210;
+let port = process.env.PORT||9120;
 let Mongo = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
--- node/bookmyshowapi/controller/dbcontroller.js
+++ node/bookmyshowapi/controller/dbcontroller.js
@@ -1,8 +1,8 @@
 let mongo = require('mongodb');
 let {MongoClient} = require('mongodb');
 let mongoUrl = "mongodb://127.0.0.1:27017";
-//"mongodb+srv://Test:abc123@cluster0.f8vmc.mongodb.net/?retryWrites=true&w=majority"
 //const uri="mongodb+srv://parikshitreddy:6yYW3XqfVo4bJyqU@cluster0.tnlzcyi.mongodb.net/?retryWrites=true&w=majority"
+"mongodb+srv://parikshitreddy5915:uqkNLlJ1nSaM2GHn@cluster0.tnlzcyi.mongodb.net/?retryWrites=true&w=majority"
 let client = new MongoClient(mongoUrl)
 
 async function dbConnect(){
